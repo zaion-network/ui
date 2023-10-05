a3744e4c3b4548300b84243f75ca23f387fe078a
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-10-06 00:25:25 +0200

    esporta PopUpMenu

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { App } from "./App";
 export { UIDesign } from "./UIDesign";
 export { UserAgent } from "./UserAgent";
 export { Dom } from "./Dom";
-export type { iUserAgentInfo } from "./UserAgent";
+export { PopUpMenu } from "./UIDesignComponents/PopUpMenu";
