2c9d206edb095d5ccd4a920bfa65df4e28d7389f
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-10-23 16:26:50 +0200

    esporta Pager

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { UIDesign } from "./UIDesign";
 export { UserAgent } from "./UserAgent";
 export { Dom } from "./Dom";
 export { PopUpMenu } from "./UIDesignComponents/PopUpMenu";
+export { Pager } from "./Pager";
