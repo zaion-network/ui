77d3d2566b7940667c46b268812ced93022a7f8e
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-10-30 14:07:28 +0100

    edita string

--- src/utils/tempoTrascorso.ts
+++ src/utils/tempoTrascorso.ts
@@ -23,7 +23,7 @@ export function tempoTrascorso(dataUnix: number, lingua: "it" | "en"): string {
   } else {
     if (anni >= 1) return `${anni} ${anni === 1 ? "day" : "days"} ago`;
     if (settimane >= 1)
-      return `${settimane} ${settimane === 1 ? "week" : "weeka"} ago`;
+      return `${settimane} ${settimane === 1 ? "week" : "weeks"} ago`;
     if (giorni >= 1) return `${giorni} ${giorni === 1 ? "day" : "days"} ago`;
     if (ore >= 1) return `${ore} ${ore === 1 ? "hour" : "hours"} ago`;
     if (minuti >= 1)
