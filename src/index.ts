295cfc96f95849c95555a711eef05239e45f8479
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-10-30 21:40:50 +0100

    exports iUIDesign

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { UserAgent } from "./UserAgent";
 export { Dom } from "./Dom";
 export { PopUpMenu } from "./UIDesignComponents/PopUpMenu";
 export { Pager } from "./Pager";
+export type { iUIDesign, Node, UINode } from "./UIDesign";
